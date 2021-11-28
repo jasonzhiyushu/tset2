9f90fbfe55d3aa8c41e49dbce3e6952a18b4d952
ZhiYushu <yushuzhi96@gmail.com>
2021-11-28 10:41:59 +0800

    master brance new code

--- src/com/zhiyushu/git/test.java
+++ src/com/zhiyushu/git/test.java
@@ -5,5 +5,6 @@ public class test {
         System.out.println("Tesgin");
         System.out.println("Tesgin");
         System.out.println("Tesgin");
+        System.out.println("This is master branch new code");
     }
 }
